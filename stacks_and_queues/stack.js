7c6d65f4ab69a7b76be1f0c9d44f9d30623e06e7
Sean Corbett <sean.corbett@umontana.edu>
2017-10-26 19:22:45 -0600

    added popSearch

--- stacks_and_queues/stack.js
+++ stacks_and_queues/stack.js
@@ -17,14 +17,25 @@ class Stack{
     this.size -= 1
     return item
   }
+  popSearch(searchItem){
+    const tempData = [].concat(this.data)
+    for(let i=this.size-1; i>= 0; i-=1){
+      const dataItem = tempData.pop()
+      if(dataItem === searchItem){
+        return dataItem
+      }
+    }
+    return "ERR: Item not found."
+  }
 
 }
 
 const stack = new Stack(5);
 
 for(let i=0; i<6; i++){
-  console.log(stack.push(i))
+  stack.push(i)
 }
 
 
-console.log(stack.pop())
+console.log("I FOUND: ",stack.popSearch(2))
+console.log("I DIDN'T FIND: ",stack.popSearch(2))
