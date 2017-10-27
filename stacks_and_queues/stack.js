0bf352321554251efe17dd8913d1f324e95a530f
Sean Corbett <sean.corbett@umontana.edu>
2017-10-26 19:40:00 -0600

    popSearch fancy

--- stacks_and_queues/stack.js
+++ stacks_and_queues/stack.js
@@ -19,13 +19,7 @@ class Stack{
   }
   popSearch(searchItem){
     const tempData = [].concat(this.data)
-    for(let i=this.size-1; i>= 0; i-=1){
-      const dataItem = tempData.pop()
-      if(dataItem === searchItem){
-        return dataItem
-      }
-    }
-    return "ERR: Item not found."
+    return tempData.find((item) => searchItem === item ) || "ERR: Item not found."
   }
 
 }
@@ -38,4 +32,4 @@ for(let i=0; i<6; i++){
 
 
 console.log("I FOUND: ",stack.popSearch(2))
-console.log("I DIDN'T FIND: ",stack.popSearch(2))
+console.log("I DIDN'T FIND: ",stack.popSearch(7))
